3a7a89a6e119c25cda902743ccea19e892c9ee55
shaista78 <shaistasulthana78@gmail.com>
2021-09-13 14:48:35 +0530

    update2

--- unit-3/sprint-2/evaluation/components/sidebar.js
+++ unit-3/sprint-2/evaluation/components/sidebar.js
@@ -1,4 +1,10 @@
 function sidebar(){
-    return ` <div id="sidebar">
-    `
+    return `<div id="sidebar">
+    <h3> <a href="/">Home</a></h3>
+    <div id="option">
+    <h3> <a href="/search.html">Search</a></h3>
+    <h3> <a href="/news.html">News</a></h3>
+    </div>
+    </div>`
 }
+export default sidebar;
