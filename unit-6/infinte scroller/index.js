56de86b3d60fd326bb048d1edc91018298c16eb8
shaista78 <shaistasulthana78@gmail.com>
2021-12-26 21:53:43 +0530

    updates

--- unit-6/infinte scroller/index.js
+++ unit-6/infinte scroller/index.js
@@ -1,13 +1,13 @@
-var item = document.querySelector("#container")
-var items = 1;
+var item = document.getElementById("container")
+var count = 1;
 var load = function(){
     for(var i=0;i<25;i++){
         var item1 = document.createElement('p');
-        item1.innerText=`Masai Students ${items++}`;
+        item1.innerText=`Masai Students ${count++}`;
         item.appendChild(item1)
     }
 }
-item.addEventListener('scroll',function(){
+document.getElementById("container").addEventListener('scroll',function(){
     if(item.scrollTop + item.clientHeight >= item.scrollHeight){
         load();
     }
