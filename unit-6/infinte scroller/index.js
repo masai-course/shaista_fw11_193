a91caa107371897392e5c7a5665a8ea078cec5ba
shaista78 <shaistasulthana78@gmail.com>
2022-01-12 22:54:40 +0530

    upadte

--- unit-6/infinte scroller/index.js
+++ unit-6/infinte scroller/index.js
@@ -1,9 +1,9 @@
 var item = document.getElementById("container")
-var count = 1;
+var counter = 1;
 var load = function(){
     for(var i=0;i<25;i++){
         var item1 = document.createElement('p');
-        item1.innerText=`Masai Students ${count++}`;
+        item1.innerText=`Masai Students ${counter++}`;
         item.appendChild(item1)
     }
 }
